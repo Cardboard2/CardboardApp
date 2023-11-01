36323b03f514035dadf393f8c8a5f4fcae20458f
anguyen9630 <72194699+anguyen9630@users.noreply.github.com>
2023-11-01 23:13:47 +1100

    Install dependency

---
+++ src/app/payment/page.tsx
@@ -0,0 +1,8 @@
+export default async function Payment() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center bg-amber-200 text-amber-950">
+        
+      </main>
+    );
+  }
+  

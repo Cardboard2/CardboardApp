75b798b199bc9fe09ccb05d4d1616f6c2a5eed17
anguyen9630 <72194699+anguyen9630@users.noreply.github.com>
2023-11-05 15:59:07 +1100

    Remove dialog library

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, Dispatch, SetStateAction, Fragment } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
+import { Transition } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 import CardboardLogo from './CardboardLogo.tsx'

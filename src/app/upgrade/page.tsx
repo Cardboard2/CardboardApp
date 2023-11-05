43292986738491eaaa6710a4a7ef6b0fc5e6c719
anguyen9630 <72194699+anguyen9630@users.noreply.github.com>
2023-11-05 16:41:28 +1100

    refactor TierSelect

--- src/app/upgrade/page.tsx
+++ src/app/upgrade/page.tsx
@@ -7,7 +7,7 @@ import { Transition } from '@headlessui/react';
 import { TierSelect } from "./_components/TierSelect.tsx"
 import { Header } from "../_components/Header.tsx"
 import { PaymentProps } from '../_components/Utils.tsx';
-import { PaymentHandler } from './_components/PaymentHandler.tsx';
+//import { PaymentHandler } from './_components/PaymentHandler.tsx';
 
 //import { SessionProvider } from "next-auth/react"
 
@@ -25,15 +25,6 @@ export default function Upgrade() {
 
   if (!paymentState)
     return;
-  
-  const ref = React.createRef<HTMLDivElement>()
-  const SelectComponent = React.forwardRef<HTMLDivElement, PaymentProps>((props , forwardedRef) => {
-    return (
-      <div ref={forwardedRef}>
-        <TierSelect paymentState={props}/>
-      </div>
-    )
-  });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-amber-200 text-amber-950">
@@ -41,19 +32,7 @@ export default function Upgrade() {
         <Header/>
       </div>
       <div className='flex z-0 w-full items-center justify-center'>
-        <Transition
-            as={Fragment}
-            show={selectingTier}
-            enter="transform duration-700 transition ease-in-out"
-            enterFrom="-translate-y-full opacity-0 scale-95"
-            enterTo="-translate-y-0 opacity-100 scale-100"
-            leave="transform duration-700 transition ease-in-out"
-            leaveFrom="-translate-y-0 opacity-100 scale-100 "
-            leaveTo="-translate-y-full opacity-0 scale-95 "
-          > 
-          <SelectComponent ref={ref} {...paymentState}/>
-        </Transition>
-        
+         <TierSelect paymentState={paymentState}/>
       </div>
     </main>
   );

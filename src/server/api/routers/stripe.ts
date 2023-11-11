44817f53212fb4cc5b19554f31a2edbb672a8669
anguyen9630 <72194699+anguyen9630@users.noreply.github.com>
2023-11-11 23:12:27 +1100

    export getbaseurl function for stripe

--- src/server/api/routers/stripe.ts
+++ src/server/api/routers/stripe.ts
@@ -8,12 +8,12 @@ import {
 import { env } from "~/env.mjs";
 
 import Stripe from 'stripe';
+import { getBaseUrl } from '~/trpc/shared';
 
 const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16",
   typescript: true
 });
-
 
 export const stripeRouter = createTRPCRouter({
   createSubscription: protectedProcedure
@@ -27,8 +27,8 @@ export const stripeRouter = createTRPCRouter({
             }
           ],
           ui_mode: "hosted",
-          success_url: 'http://localhost:3000/checkout/success?session_id={CHECKOUT_SESSION_ID}',
-          cancel_url: 'http://localhost:3000/checkout/cancelled?session_id={CHECKOUT_SESSION_ID}'
+          success_url: getBaseUrl() + "/checkout/success?session_id={CHECKOUT_SESSION_ID}",
+          cancel_url: getBaseUrl() + "/checkout/cancelled?session_id={CHECKOUT_SESSION_ID}"
           
         });
     }),

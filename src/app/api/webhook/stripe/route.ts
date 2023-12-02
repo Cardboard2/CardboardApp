bc994895439ed22a7cb8df62cfc54d021797d315
wontonz-1 <59831427+wontonz-1@users.noreply.github.com>
2023-12-02 18:21:18 +1100

    Updated stripe webhook to update user tier expiry date

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -1,4 +1,3 @@
-
 import Stripe from "stripe";
 import { PrismaClient } from "@prisma/client";
 import { headers } from "next/headers";
@@ -9,58 +8,76 @@ import { type NextRequest, NextResponse } from "next/server";
 const db = new PrismaClient();
 
 const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-    apiVersion: "2023-10-16",
-    typescript: true
-  });
+  apiVersion: "2023-10-16",
+  typescript: true,
+});
 
 export async function POST(req: NextRequest) {
-  const priceMap = new Map<string, string>([
-    ["199", "tier-normal"],
-    ["1799", "tier-normal"],
-    ["499", "tier-whale"],
-    ["5999", "tier-whale"],
-  ]); 
-   
+  const priceMap = new Map<string, { tier: string; renewMonths: number }>([
+    ["199", { tier: "tier-normal", renewMonths: 1 }],
+    ["1799", { tier: "tier-normal", renewMonths: 12 }],
+    ["499", { tier: "tier-whale", renewMonths: 1 }],
+    ["5999", { tier: "tier-whale", renewMonths: 12 }],
+  ]);
+
   const body = await req.text();
   const signature = headers().get("stripe-signature");
-  let event : Stripe.Event | null = null;
+  let event: Stripe.Event | null = null;
   try {
-    event = stripe.webhooks.constructEvent(body, signature!, env.STRIPE_WEBHOOK_SECRET);
-    switch (event?.type){
-      case "checkout.session.completed":
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature!,
+      env.STRIPE_WEBHOOK_SECRET,
+    );
 
-        if (!event.data.object.customer_email || !event.data.object.amount_total)
+    switch (event?.type) {
+      case "checkout.session.completed":
+        if (
+          !event.data.object.customer_email ||
+          !event.data.object.amount_total
+        )
           throw new Error("Invalid checkout session data");
-        
+
         const userEmail = event.data.object.customer_email;
         const paymentAmount = String(event.data.object.amount_total);
-        const tierId = priceMap.get(paymentAmount);
+        const tierObject = priceMap.get(paymentAmount);
+
+        let expiryDate = new Date();
+        if (tierObject !== undefined)
+          expiryDate.setMonth(expiryDate.getMonth() + tierObject?.renewMonths);
 
         const updatedUser = await db.user.update({
-          where : {email: userEmail},
-          data  : {tierId: tierId},
+          where: { email: userEmail },
+          data: {
+            tierId: tierObject?.tier,
+            tierExpiry: expiryDate,
+          },
         });
 
-        if (!updatedUser)
-          throw new Error("Failed to update user");
+        if (!updatedUser) throw new Error("Failed to update user");
 
         const newPayment = await db.payment.create({
           data: {
             userId: updatedUser.id,
-            id    : event.data.object.id,
-            tierId: tierId!,
-            status: event.data.object.status!
-          }
+            id: event.data.object.id,
+            tierId: tierObject?.tier!,
+            status: event.data.object.status!,
+          },
         });
-        
-        if (!newPayment)
-          throw new Error("Failed to store payment!");
-    };
+
+        if (!newPayment) throw new Error("Failed to store payment!");
+
+      case "customer.subscription.deleted":
+      // Make them pleb again subscription ends
+      // Update expiry date to nothing?
+
+      // Need to make another case for subscription updated
+    }
   } catch (err) {
-    const errMsg = err instanceof Error ? err.message : "Unknown Error..." 
+    const errMsg = err instanceof Error ? err.message : "Unknown Error...";
     console.error(`Webhook processing failed: ${errMsg}`);
     return NextResponse.json({ error: errMsg }, { status: 400 });
-  };
+  }
 
   return NextResponse.json({ received: true });
-};
+}

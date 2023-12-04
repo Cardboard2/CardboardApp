2d79d93f0c7f569b49fb23fa5600d9fa255b161d
wontonz-1 <59831427+wontonz-1@users.noreply.github.com>
2023-12-04 21:37:33 +1100

    Update DisplayFiles.tsx

--- src/app/dashboard/_components/DisplayFiles.tsx
+++ src/app/dashboard/_components/DisplayFiles.tsx
@@ -16,6 +16,19 @@ import {
 } from "@heroicons/react/24/outline";
 import type { Dispatch, SetStateAction } from "react";
 import { UsageBarProps } from "~/app/_components/UsageBarProps";
+import { type SubmitHandler, useForm } from "react-hook-form";
+import { FormInput } from "./FileDetail";
+
+function readFile(file: File) {
+  return new Promise((resolve, reject) => {
+    const fr = new FileReader();
+    fr.onload = () => {
+      resolve(fr.result);
+    };
+    fr.onerror = reject;
+    fr.readAsBinaryString(file);
+  });
+}
 
 import Notification from "~/app/_components/Notification";
 interface DisplayProps {
@@ -114,7 +127,7 @@ export function DisplayFiles(props: {
     deleteFileAPI.mutate({
       request: { name: name, folderId: currFolderId },
     });
-    
+
     for (let i = 0; i < currItems.length; i++) {
       if (currItems[i]?.id == id) {
         currItems.splice(i, 1);
@@ -175,6 +188,56 @@ export function DisplayFiles(props: {
     }
   }, []);
 
+  const { register, handleSubmit } = useForm<FormInput>();
+
+  const uploadFile = api.aws.uploadFile.useMutation({
+    onSuccess: (data) => {
+      if (data && data.status == "success") {
+        console.log(data.usage);
+        getFiles.mutate({ folderId: currFolderId });
+        props.usageBarProps.setUsageBarUsage(data.usage.userUsage);
+        props.usageBarProps.setUsageBarTotal(data.usage.totalStorage);
+
+        setNotificationTitle("File upload successfully!");
+        setNotificationMessage("File has been uploaded");
+        setNotificationSuccess(true);
+        setNotificationVisible(true);
+        setTimeout(function () {
+          setNotificationVisible(false);
+        }, 3000);
+      } else {
+        setNotificationTitle("File upload failed..");
+        setNotificationMessage("Error detected...");
+        setNotificationSuccess(false);
+        setNotificationVisible(true);
+        setTimeout(function () {
+          setNotificationVisible(false);
+        }, 10000);
+      }
+    },
+  });
+
+  const onSubmit: SubmitHandler<FormInput> = async (data) => {
+    for (const currFile of data.files) {
+      if (!currFile.name || !currFile.size || !currFile.type) return;
+
+      await new Promise((r) => setTimeout(r, 1000));
+      await readFile(currFile).then((file) => {
+        const fileData = {
+          name: currFile.name,
+          size: currFile.size,
+          type: currFile.type,
+          folderId: currFolderId,
+        };
+        const rawData = file as Blob;
+        const encryptedData = btoa(String(rawData));
+        uploadFile.mutate({ file: encryptedData, metadata: fileData });
+      });
+    }
+
+    // create an api that sends the file to the aws upload feature
+  };
+
   function DisplayIndividualFile(file: FileDetail, displayProps: DisplayProps) {
     const itemBaseClassname =
       "flex items-center mb-2 \
@@ -183,7 +246,7 @@ export function DisplayFiles(props: {
 
     if (file.objectType == "Folder")
       return (
-        <div id={file.id} className={itemBaseClassname}>
+        <div id={file.id} className={itemBaseClassname} key={file.id}>
           <div
             className="h-full w-1/12 cursor-pointer p-1"
             onClick={() => {
@@ -207,7 +270,7 @@ export function DisplayFiles(props: {
       );
     else
       return (
-        <div id={file.id} className={itemBaseClassname}>
+        <div id={file.id} className={itemBaseClassname} key={file.id}>
           <div
             className="h-full w-1/12 cursor-pointer p-1"
             onDoubleClick={() =>
@@ -293,26 +356,38 @@ export function DisplayFiles(props: {
       ) : (
         <></>
       )}
-      <div className="hidden lg:block h-[8%] w-full border-b-2 border-amber-800 bg-amber-300 p-5 lg:p-8"></div>
+      <div className="hidden h-[8%] w-full border-b-2 border-amber-800 bg-amber-300 p-5 lg:block lg:p-8"></div>
       <div className={`h-[92%] w-full p-2`}>
         {displayFiles ? (
           <div className="relative h-full w-full overflow-y-auto">
             <DisplayFileList displayProps={displayProps} />
             <button
-                onClick={()=>props.dashboardProps.setCreationOpen(!props.dashboardProps.creationOpen)}
-                type="button"
-                className="fixed bottom-5 left-5 lg:left-80 duration-300 active:opacity-80 rounded-full bg-amber-600 p-2 text-white shadow-xl hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600"
-              >
+              onClick={() =>
+                props.dashboardProps.setCreationOpen(
+                  !props.dashboardProps.creationOpen,
+                )
+              }
+              type="button"
+              className="fixed bottom-5 left-5 rounded-full bg-amber-600 p-2 text-white shadow-xl duration-300 hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600 active:opacity-80 lg:left-80"
+            >
               <DocumentArrowUpIcon className="h-10 w-10" aria-hidden="true" />
             </button>
 
             <button
-                onClick={()=>{createFolder()}}
-                type="button"
-                className="fixed bottom-20 left-5 lg:left-80 duration-300 active:opacity-80 rounded-full bg-amber-600 p-2 text-white shadow-xl hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600"
-              >
+              onClick={() => {
+                createFolder();
+              }}
+              type="button"
+              className="fixed bottom-20 left-5 rounded-full bg-amber-600 p-2 text-white shadow-xl duration-300 hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600 active:opacity-80 lg:left-80"
+            >
               <FolderPlusIcon className="h-10 w-10" aria-hidden="true" />
             </button>
+            <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+              <input {...register("files")} type="file" id="upload" multiple />
+              <button className=" float-right inline-flex rounded-md bg-amber-400 px-3 py-2 text-sm font-semibold  text-gray-900  shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
+                Upload File
+              </button>
+            </form>
           </div>
         ) : (
           <div className="flex h-full w-full items-center justify-center">
